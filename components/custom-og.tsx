545aa9e375c4edd4a906f275aad0fc9d0e8dce4a
DylWithlt <driscoll_dylan@yahoo.com>
2025-09-16 16:13:02 -0400

    added logo to social image

--- components/custom-og.tsx
+++ components/custom-og.tsx
@@ -18,69 +18,121 @@ export const customImage: SocialImageOptions["imageStructure"] = ({
   fonts: SatoriOptions["fonts"]
   fileData: QuartzPluginData
 }) => {
-  const fontBreakPoint = 22
+  const fontBreakPoint = 24
   const useSmallerFont = title.length > fontBreakPoint
   const { colorScheme } = userOpts
+
+  // Colors from config
+  const colors = cfg.theme.colors[colorScheme]
 
   return (
     <div
       style={{
         display: "flex",
-        flexDirection: "row",
-        justifyContent: "flex-start",
-        alignItems: "center",
         height: "100%",
         width: "100%",
+        backgroundColor: colors.light,
+        padding: "3rem",
+        fontFamily: fonts[0].name,
       }}
     >
+      {/* Left accent bar */}
       <div
         style={{
           display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "100%",
-          width: "100%",
-          backgroundColor: cfg.theme.colors[colorScheme].light,
-          flexDirection: "column",
-          gap: "2.5rem",
-          paddingTop: "2rem",
-          paddingBottom: "2rem",
-        }}
-      >
-        <p
-          style={{
-            color: cfg.theme.colors[colorScheme].dark,
-            fontSize: useSmallerFont ? 70 : 82,
-            marginLeft: "4rem",
-            textAlign: "center",
-            marginRight: "4rem",
-            fontFamily: fonts[0].name,
-          }}
-        >
-          {title}
-        </p>
-        <p
-          style={{
-            color: cfg.theme.colors[colorScheme].dark,
-            fontSize: 44,
-            marginLeft: "8rem",
-            marginRight: "8rem",
-            lineClamp: 3,
-            fontFamily: fonts[1].name,
-          }}
-        >
-          {description}
-        </p>
-      </div>
-      <div
-        style={{
-          height: "100%",
-          width: "2vw",
-          position: "absolute",
-          backgroundColor: cfg.theme.colors[colorScheme].tertiary,
-          opacity: 0.85,
+          width: "1.2rem",
+          backgroundColor: colors.tertiary,
+          marginRight: "3rem",
+          borderRadius: "8px",
         }}
       />
+
+      {/* Main content container */}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          flex: 1,
+        }}
+      >
+        {/* Logo */}
+        <div
+          style={{
+            display: "flex",
+            marginBottom: "2.5rem",
+          }}
+        >
+          <img
+            src="https://dylwithlt.github.io/DylsScriptingArticles/static/icon.png"
+            width={80}
+            height={80}
+            style={{
+              borderRadius: "8px",
+              boxShadow: "0 4px 12px rgba(0,0,0,0.1)",
+            }}
+          />
+        </div>
+
+        {/* Title */}
+        <div
+          style={{
+            display: "flex",
+            marginBottom: "1.5rem",
+          }}
+        >
+          <div
+            style={{
+              color: colors.dark,
+              fontSize: useSmallerFont ? "3.5rem" : "4.2rem",
+              fontWeight: 700,
+              lineHeight: 1.1,
+              letterSpacing: "-0.5px",
+            }}
+          >
+            {title}
+          </div>
+        </div>
+
+        {/* Description */}
+        {description && (
+          <div
+            style={{
+              display: "flex",
+              marginBottom: "2rem",
+            }}
+          >
+            <div
+              style={{
+                color: colors.gray || "#666666",
+                fontSize: "2rem",
+                fontWeight: 400,
+                lineHeight: 1.4,
+                maxWidth: "90%",
+              }}
+            >
+              {description}
+            </div>
+          </div>
+        )}
+
+        {/* Bottom decorative element */}
+        <div
+          style={{
+            display: "flex",
+            marginTop: "auto",
+          }}
+        >
+          <div
+            style={{
+              width: "120px",
+              height: "6px",
+              backgroundColor: colors.tertiary,
+              borderRadius: "3px",
+            }}
+          />
+        </div>
+      </div>
     </div>
   )
 }
